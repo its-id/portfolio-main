4867f1810111d422a1f3390d1e382a98d66beee3
its-id <ik.dana24@gmail.com>
2024-09-27 00:37:23 -0400

    change the name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,7 @@ export default function HomePage(): JSX.Element {
 						}}
 						className="text-gray-500 dark:text-white text-5xl sm:text-6xl md:text-6xl lg:text-8xl tracking-tight font-extrabold">
 						Hey <span className="inline-block origin-70 hover:(animate-wave)">👋</span>{' '}
-						I&apos;m Indrakant, <br className="sm:block" />a{' '}
+						I&apos;m Dana, <br className="sm:block" />a{' '}
 						<Pill.Standard className="mt-4">developer</Pill.Standard>
 					</Animate>
 
